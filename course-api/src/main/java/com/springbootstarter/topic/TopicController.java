377819cde19eb73f3d376d0acaa494eb6f97f75f
Ryan Manchester <ryanm17@gmail.com>
2023-08-22 13:47:19 -0400

    hard codes sample data for json mapping example

--- course-api/src/main/java/com/springbootstarter/topic/TopicController.java
+++ course-api/src/main/java/com/springbootstarter/topic/TopicController.java
@@ -1,5 +1,8 @@
 package com.springbootstarter.topic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
 	
 	@RequestMapping("/topics")
-	public String getAllTopics() {
-		return "All Topics";
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(
+				new Topic("1", "Spring Framework", "Spring Description"),
+				new Topic("2", "Java Basics", "Java for beginners"),
+				new Topic("3", "Selenium Advanced", "Advanced concepts for Selenium")
+				
+				);
 	}
 
 }
