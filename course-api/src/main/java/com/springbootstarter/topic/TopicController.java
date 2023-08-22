5563de91d7b9839fee21bf3c394a5d77b63ee07a
Ryan Manchester <ryanm17@gmail.com>
2023-08-22 14:13:35 -0400

    refactors topic controller logic as service logic

--- course-api/src/main/java/com/springbootstarter/topic/TopicController.java
+++ course-api/src/main/java/com/springbootstarter/topic/TopicController.java
@@ -3,20 +3,19 @@ package com.springbootstarter.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-				new Topic("1", "Spring Framework", "Spring Description"),
-				new Topic("2", "Java Basics", "Java for beginners"),
-				new Topic("3", "Selenium Advanced", "Advanced concepts for Selenium")
-				
-				);
+		return topicService.getAllTopics();
 	}
 
 }
---
+++ course-api/src/main/java/com/springbootstarter/topic/TopicService.java
@@ -0,0 +1,19 @@
+package com.springbootstarter.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	List<Topic> topics = Arrays.asList(
+			new Topic("1", "Spring Framework", "Spring Description"),
+			new Topic("2", "Java Basics", "Java for beginners"),
+			new Topic("3", "Selenium Advanced", "Advanced concepts for Selenium")
+			
+			);
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+}
