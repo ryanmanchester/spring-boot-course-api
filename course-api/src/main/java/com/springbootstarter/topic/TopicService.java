24b0e67f5cc3e58a7714507deeb65fc0becc8676
Ryan Manchester <ryanm17@gmail.com>
2023-08-24 15:46:24 -0400

    refactors crud operations using apache derpy and crud repository

--- course-api/src/main/java/com/springbootstarter/topic/TopicService.java
+++ course-api/src/main/java/com/springbootstarter/topic/TopicService.java
@@ -3,6 +3,7 @@ package com.springbootstarter.topic;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -30,28 +31,26 @@ public class TopicService {
 		return topics;
 	}
 	
-	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id))
-					   .findFirst().get();
-	}
+	
 	
 	public void addTopic(Topic topic) {
 		//topics.add(topic);
 		topicRepository.save(topic);
 	}
+	
+	public Optional<Topic> getTopic(String id) {
+		//return topics.stream().filter(t -> t.getId().equals(id))
+					   //.findFirst().get();
+		return topicRepository.findById(id);
+	}
 
 	public void updateTopic(String id, Topic topic) {
-		for(int i = 0; i < topics.size(); i++) {
-			Topic t = topics.get(i);
-			if(t.getId().equals(id)) {
-				topics.set(i, topic);
-				break;
-			}
-		}
+		topicRepository.save(topic);
 		
 	}
 
 	public void deleteTopic(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
+		//topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.deleteById(id);
 	}
 }
